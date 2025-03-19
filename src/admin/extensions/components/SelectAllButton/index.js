7c3822cf57e9dc0dca042e16d1d8625524a75118
Ezequiel Emanuel Ramirez <ezequielram@gmail.com>
2025-03-19 12:49:50 -0300

    Enhance SelectAllButton with notification feedback
    - Integrated notification system to provide user feedback on selection and dispatch actions.
    - Updated state management to track selection status and improve user experience.
    - Refactored simulateSelection function for better clarity and functionality.
    - Added error handling notifications for API interactions.

--- src/admin/extensions/components/SelectAllButton/index.js
+++ src/admin/extensions/components/SelectAllButton/index.js
@@ -2,49 +2,69 @@ import React, { useState } from "react";
 import { Button } from "@strapi/design-system";
 import { Boolean, PaperPlane } from "@strapi/icons";
 import { useLocation } from "react-router-dom";
+import { useNotification } from '@strapi/helper-plugin';
 import axios from 'axios';
 
 const SelectAllButton = () => {
+  const toggleNotification = useNotification();
   const location = useLocation();
   const isEnvioView = /\/content-manager\/collection-types\/api::envio\.envio/.test(location.pathname);
   const [selectedEntries, setSelectedEntries] = useState([]);
+  const [areAllSelected, setAreAllSelected] = useState(false);
 
   if (!isEnvioView) {
     return null;
   }
+  
+  const simulateSelection = (checked) => {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach(checkbox => {
+      if (checkbox instanceof HTMLInputElement) {
+        checkbox.checked = checked;
+      }
+    });
+  };
 
   const handleSelectAll = async () => {
     try {
+      if (areAllSelected) {
+        // Si ya están seleccionadas, deseleccionar todas
+        setSelectedEntries([]);
+        simulateSelection(false); // Desmarcar todos los checkboxes
+        setAreAllSelected(false);
+      } else {
       // Hacer una llamada a la API para obtener todas las entradas
       const response = await axios.get('/api/envios');
       const allEntries = response.data.data;
 
       // Actualizar el estado local con los IDs seleccionados
       setSelectedEntries(allEntries);
-
       // Simular la selección en el Content Manager
-      simulateSelection();
+      simulateSelection(true);
+      setAreAllSelected(true);
 
-      console.log('Todas las entradas seleccionadas:', allEntries);
+      toggleNotification({
+        type: 'success',
+        message: 'Todas las entradas seleccionadas',
+      });
+      }
     } catch (error) {
       console.error('Error al obtener las entradas:', error);
+      toggleNotification({
+        type: 'warning',
+        message: 'Error al obtener las entradas',
+      });
     }
   };
 
-  const simulateSelection = () => {
-    // seleccionar checkboxes en la tabla del Content Manager
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    // Simular la selección de todas las entradas
-    checkboxes.forEach(checkbox => {
-      if (checkbox instanceof HTMLInputElement) {
-        checkbox.checked = true;
-      }
-    });
-  };
+  
   
   const handleEnviarADespacho = async () => {
     if (selectedEntries.length === 0) {
-      alert('No hay entradas seleccionadas.');
+      toggleNotification({
+        type: 'warning',
+        message: 'No hay entradas seleccionadas.',
+      });
       return;
     }
 
@@ -53,13 +73,18 @@ const SelectAllButton = () => {
       for (const entry of selectedEntries) {
         const response = await axios.post('/api/dispatches', { data: entry }); 
         console.log('response', response);
-        console.log(`Entrada ${entry.id} enviada a despacho.`);
       }
 
-      alert('Entradas enviadas a despacho correctamente.');
+        toggleNotification({
+        type: 'success',
+        message: 'Entradas enviadas a despacho correctamente.',
+      });
     } catch (error) {
       console.error('Error al enviar a despacho:', error);
-      alert('Hubo un error al enviar las entradas a despacho.');
+      toggleNotification({
+        type: 'warning',
+        message: 'Hubo un error al enviar las entradas a despacho.',
+      });
     }
   };
 
@@ -73,9 +98,10 @@ const SelectAllButton = () => {
         Seleccionar Todas
       </Button>
       <Button
-        variant="primary"
+        variant={selectedEntries.length > 0 ? 'primary' : 'secondary'}
         startIcon={<PaperPlane />}
         onClick={handleEnviarADespacho}
+        disabled={selectedEntries.length === 0}
       >
         Enviar a Despacho
       </Button>
